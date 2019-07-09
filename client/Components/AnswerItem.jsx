1715bfbf60f7c07e814a68cba06b9552ce1508dc
chrisfauries <45318711+chrisfauries@users.noreply.github.com>
2019-07-09 00:27:05 -0500

    hooked up to mongo server

--- client/Components/AnswerItem.jsx
+++ client/Components/AnswerItem.jsx
@@ -7,7 +7,7 @@ function AnswerItem(props) {
     <div className='row'>
       <div className="col-2"></div>
       <div className='col-9'>
-        <img src={`https://lowesproject.s3.amazonaws.com/badges/${badgeName}.jpg`} alt="Company Lego(if I do this)" />
+        {badgeName ? <img src={`https://lowesproject.s3.amazonaws.com/badges/${badgeName}.jpg`} alt="Company Lego(if I do this)" /> : <></>}
         <div className='mt-2'><small className='text-muted'>{author} on {date}</small></div>
         <div className='mt-2'>{text}</div>
         <div className="row mt-2">
--- server/index.js
+++ server/index.js
@@ -3,7 +3,6 @@ const express = require('express');
 const path = require('path');
 const app = express();
 const db = require('../db/index.js');
-const data = require('./dummyData.json');
 const axios = require('axios');
 const PORT = process.env.PORT || 3000
 
@@ -11,13 +10,11 @@ app.use(express.json());
 app.use('/', express.static(path.join(__dirname, '../public')));
 
 app.get('/api/product/:id', (req, res) => {
-    res.send(data)
+    const { id } = req.params;
+    db.Product.find({product_id: id}).then(data => res.send(data[0]))
 });
 
 
-app.put('/saveImage/:url', (req,res) => {
-    console.log(req.body);
-
-})
+app.get('')
 
 app.listen(PORT, () => console.log('now listening on port: ' + PORT));
