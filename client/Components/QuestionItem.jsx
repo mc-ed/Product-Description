658ec0f38a29e4efb04906f729d0aa480294da89
chrisfauries <45318711+chrisfauries@users.noreply.github.com>
2019-07-14 01:24:16 -0500

    refactor using reactBootstrap and css Modules

--- client/Components/QuestionItem.jsx
+++ client/Components/QuestionItem.jsx
@@ -1,58 +1,69 @@
-import React from "react";
-import AnswerItem from "./AnswerItem.jsx";
+import React, { useState } from 'react';
+import AnswerItem from './AnswerItem.jsx';
 import uuidv4 from 'uuid/v4';
+import styles from '../styles/QuestionItem.less';
+import buttons from '../styles/buttons.less';
 
 function QuestionItem(props) {
-  let questionRef = null;
-  function toggleShow () {
-    let rotateCaret = questionRef.querySelector('span.downCaret').parentElement;
-    if(questionRef.classList.contains('show')) {
-      questionRef.classList.remove('show', 'expand');
-      rotateCaret.classList.remove('rotateCaret');
-    } else {
-      questionRef.classList.add('show', 'expand');
-      rotateCaret.classList.add('rotateCaret');
-    }
-  }
+	const [questionRef, setQuestionRef] = useState();
+	function toggleShow() {
+		let rotateCaret = questionRef.querySelector(`span.${styles.downCaret}`).parentElement;
+		if (questionRef.classList.contains(styles.show)) {
+			questionRef.classList.remove(styles.show, styles.expand);
+			rotateCaret.classList.remove(styles.rotateCaret);
+		} else {
+			questionRef.classList.add(styles.show, styles.expand);
+			rotateCaret.classList.add(styles.rotateCaret);
+		}
+	}
 
-  const { question, author, date, answers } = props.question;
-  return (
-    <div className='questionHide' ref={input => questionRef = input}>
-      <div onClick={() => {toggleShow()} } className="row" style={{cursor: 'pointer', marginTop: '8px'}}>
-        <div className="col-1 text-center down" style={{padding: '16px'}}>
-          <span className="downCaret" />
-        </div>
-        <div className="col-9">
-          <div data="question container">
-            <div className='text-lowes font-weight-bold'>{question}</div>
-            <div>
-              <small className='text-muted'>
-              {author} on {date}
-              </small>
-            </div>
-          </div>
-        </div>
-        <div className="col-2">
-        {answers.length ? (
-          <div className='text-center'>
-            <div style={{fontSize: '24px', fontWeight: 'bold'}}>{answers.length}</div>
-            <div>{answers.length > 1 ? "Answers" : "Answer"}</div>
-            <span className="lowesButton" style={{position: 'absolute', width: '250px', left: '-120px', top: '80px'}}>ANSWER THIS QUESTION</span>
-          </div>
-        ) : (
-          <span className="lowesButton" style={{position: 'absolute', width: '250px', left: '-80px', top: '8px'}}>ANSWER THIS QUESTION</span>
-        )}
-
-        </div>
-      </div>
-      <div data="All Answers container">
-        {answers.length && answers.length > 1 ? (
-          answers.map(answer => (<div key={uuidv4()}><AnswerItem answer={answer} /><hr /></div>))) :
-          answers.length === 1 ? <AnswerItem answer={answers[0]} /> :<div />
-        }
-      </div>
-    </div>
-  );
+	const { question, author, date, answers } = props.question;
+	return (
+		<div className={styles.questionHide} ref={input => setQuestionRef(input)}>
+			<div
+				onClick={() => {
+					toggleShow();
+				}}
+				className={`row ${styles.pointer} ${styles.mt8}`}>
+				<div className={`col-1 text-center down ${styles.pad16}`}>
+					<span className={styles.downCaret} />
+				</div>
+				<div className='col-9'>
+					<div className={`${styles.textLowes} font-weight-bold`}>{question}</div>
+					<div className='ml-3'>
+						<small className='text-muted'>
+							{author} on {date}
+						</small>
+					</div>
+				</div>
+				<div className='col-2'>
+					{answers.length ? (
+						<div className='text-center'>
+							<div className={styles.answerLength}>{answers.length}</div>
+							<div>{answers.length > 1 ? 'Answers' : 'Answer'}</div>
+							<span className={`${buttons.button} ${styles.answerButton}`}>ANSWER THIS QUESTION</span>
+						</div>
+					) : (
+						<span className={`${buttons.button} ${styles.answerButton}`}>ANSWER THIS QUESTION</span>
+					)}
+				</div>
+			</div>
+			<div data='All Answers container'>
+				{answers.length && answers.length > 1 ? (
+					answers.map(answer => (
+						<div key={uuidv4()}>
+							<AnswerItem answer={answer} />
+							<hr />
+						</div>
+					))
+				) : answers.length === 1 ? (
+					<AnswerItem answer={answers[0]} />
+				) : (
+					<div />
+				)}
+			</div>
+		</div>
+	);
 }
 
 export default QuestionItem;
--- client/Components/QuestionsAnswers.jsx
+++ client/Components/QuestionsAnswers.jsx
@@ -1,91 +1,71 @@
-import React from "react";
-import QuestionItem from "./QuestionItem.jsx";
+import React from 'react';
+import uuidv4 from 'uuid/v4';
+import header from '../styles/CardHeader.less';
+import QuestionItem from './QuestionItem.jsx';
+import { Accordion, Card } from 'react-bootstrap';
+import signs from '../styles/signs.less';
+import styles from '../styles/QuestionsAndAnswers.less';
+import buttons from '../styles/buttons.less';
 
 function QuestionsAnswers(props) {
-  const signToggle = document.querySelector(
-    'span[data="toggleQuestionAndAnswerSign"]'
-  );
-  {/* style={{ backgroundColor, cursor: "pointer" }} */}
-  return (
-    <div className="card">
-      <div
-        onClick={() => {
-          props.onClick(signToggle);
-        }}
-        className="card-header"
-        id="headingFour"
-        data-toggle="collapse"
-        data-target="#collapseFour"
-        aria-expanded="true"
-        aria-controls="collapseFour"
-      >
-        <span className="iconFont">{"\uEAC6 "}</span>
-        <span className="text-white font-weight-bold">Community Q & A</span>
-        <span
-          data="toggleQuestionAndAnswerSign"
-          className="float-right plusSign"
-        />
-      </div>
-      <div
-        id="collapseFour"
-        className="collapse"
-        aria-labelledby="headingFour"
-        data-parent="#accordionExample"
-      >
-        <div className="card-body">
-          <div className="row">
-            <div className="col-2 text-center">
-              <div style={{ fontSize: "24px", fontWeight: "bold" }}>
-                {props.questions.length}
-              </div>
-              <div>Question{props.questions.length > 1 ? "s" : ""}</div>
-            </div>
-            <div className="col-8">
-              <div className="font-weight-bold">Search All Questions</div>
-              <input style={{ width: "80%", height: "36px" }} type="text" />
-              <span className="lowesButton">Search</span>
-            </div>
-            <div className="col-2">
-              <div className="lowesButton">ASK A QUESTION</div>
-            </div>
-          </div>
-          <div
-            className="bg-grey"
-            style={{ height: "60px", marginTop: "10px" }}
-          >
-            <select
-              className="float-right"
-              style={{ height: "36px", margin: "10px" }}
-              name="sortQuestions"
-              id="sortQuestions"
-            >
-              <option defaultValue="MOST ANSWERED">MOST ANSWERED</option>
-              <option defaultValue="Newest to Oldest">Newest to Oldest</option>
-              <option defaultValue="Oldest to Newest">Oldest to Newest</option>
-              <option defaultValue="Answers Needed">Answers Needed</option>
-              <option defaultValue="Most Recently Answered">
-                Most Recently Answered
-              </option>
-            </select>
-          </div>
-          <div data="All questions container">
-            {props.questions.length && props.questions.length > 1 ? (
-              props.questions.map(question => (
-                <>
-                  <QuestionItem question={question} />
-                  <hr />
-                </>
-              ))
-            ) : props.questions.length === 1 ? (
-              <QuestionItem question={props.questions[0]} />
-            ) : (
-              <></>
-            )}
-          </div>
-        </div>
-      </div>
-    </div>
-  );
+	const signToggle = document.querySelector('span[data="toggleQuestionAndAnswerSign"]');
+
+	return (
+		<Card>
+			<Accordion.Toggle
+				as={Card.Header}
+				eventKey='3'
+				className={`card-header ${header.header}`}
+				onClick={() => {
+					props.onClick(signToggle);
+				}}>
+				<span className={header.icon}>{'\uEAC6 '}</span>
+				<span className='text-white font-weight-bold'>Community Q & A</span>
+				<span data='toggleQuestionAndAnswerSign' className={`float-right ${signs.plusSign}`} />
+			</Accordion.Toggle>
+			<Accordion.Collapse eventKey='3'>
+				<Card.Body>
+					<div className='row'>
+						<div className='col-2 text-center'>
+							<div className={styles.questionsNo}>{props.questions.length}</div>
+							<div>Question{props.questions.length > 1 ? 's' : ''}</div>
+						</div>
+						<div className='col-8'>
+							<div className='font-weight-bold'>Search All Questions</div>
+							<input className={styles.input} type='text' />
+							<span className={buttons.button}>Search</span>
+						</div>
+						<div className='col-2'>
+							<div className={buttons.button}>ASK A QUESTION</div>
+						</div>
+					</div>
+					<div className={`${styles.greyBG} ${styles.selectBar}`}>
+						<select className={`float-right ${styles.select}`} name='sortQuestions' id='sortQuestions'>
+							<option defaultValue='MOST ANSWERED'>MOST ANSWERED</option>
+							<option defaultValue='Newest to Oldest'>Newest to Oldest</option>
+							<option defaultValue='Oldest to Newest'>Oldest to Newest</option>
+							<option defaultValue='Answers Needed'>Answers Needed</option>
+							<option defaultValue='Most Recently Answered'>Most Recently Answered</option>
+						</select>
+					</div>
+					<div data='All questions container'>
+						{props.questions.length && props.questions.length > 1 ? (
+							props.questions.map(question => (
+								<div key={uuidv4()}>
+									<QuestionItem question={question} />
+									<hr />
+								</div>
+							))
+						) : props.questions.length === 1 ? (
+							<QuestionItem question={props.questions[0]} />
+						) : (
+							<></>
+						)}
+					</div>
+				</Card.Body>
+			</Accordion.Collapse>
+		</Card>
+	);
 }
 
 export default QuestionsAnswers;
