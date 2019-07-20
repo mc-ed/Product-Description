3ce12846b313344fe69bdc224570ef7a90532e62
chrisfauries <45318711+chrisfauries@users.noreply.github.com>
2019-07-20 08:32:16 -0500

    pm2 config for .env

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -11,7 +11,7 @@ module.exports = {
         ref: 'origin/master',
         repo: 'git@github.com:mc-ed/Product-Description.git',
         path: '~/description',
-        'post-deploy': 'npm install && pm2 startOrRestart ecosystem.config.js'
+        'post-deploy': 'npm install && npm run build && pm2 startOrRestart ecosystem.config.js'
       }
     }
   }
